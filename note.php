05bba273808ea05b0804bca55ac93bcc16966e6c
Ismaël TALBI <isumaeru1987@gmail.com>
2026-03-23 11:51:58 +0100

    Récupération des informations de l'étudiant

--- note.php
+++ note.php
@@ -27,18 +27,18 @@ $eleve = current(array_filter($data['eleves'], function($student){
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
   <link href="assets/css/note.css" rel="stylesheet" />
-  <title><?= "John Doe" ?></title>
+  <title><?= $eleve['prenom'].' '.$eleve['nom'] ?></title>
 </head>
 <body>
   <div class="page">
     <section class="student-card">
       <div class="student-photo">
-        <img src="" alt="Photo de John Doe">
+        <img src="assets/images/students/<?= $eleve['image'] ?>" alt="Photo de <?= $eleve['prenom'].' '.$eleve['nom'] ?>">
       </div>
 
       <div class="student-info">
-        <h1>John Doe</h1>
-        <div class="student-class">B1</div>
+        <h1><?= $eleve['prenom'].' '.$eleve['nom'] ?></h1>
+        <div class="student-class"><?= $eleve['classe'] ?></div>
 
         <div class="student-summary">
           <div class="summary-box">
