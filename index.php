79ec27af293a818ec71c9b65c6b5cb239cf2c00c
Ismaël TALBI <isumaeru1987@gmail.com>
2026-03-23 11:39:39 +0100

    index.php opérationnel

--- index.php
+++ index.php
@@ -45,7 +45,9 @@ $data = json_decode(file_get_contents($filepath), true);
             <?= $eleve['evaluation_globale'] ?>
           </p>
           <div>
-            <button class="btn-notes">Voir les notes</button>
+            <a href='note.php?id=<?= $eleve['id'] ?>'>
+              <button class="btn-notes">Voir les notes</button>
+            </a>
           </div>
         </div>
       </article>
