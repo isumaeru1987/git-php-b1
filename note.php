ec45457c628d2b32f682d13570efbc5707d875d1
NameraGK3M <learuiz60@gmail.com>
2026-04-02 11:30:45 +0200

    Récupération des informations de l'étudiant

--- note.php
+++ note.php
@@ -17,6 +17,8 @@ $eleve = array_filter($data['eleves'], function($student){
 ////Pour débugger :
 //var_dump($eleve); //Affiche le tableau de l'élève
 
+$test = current($eleve);
+
 ?>
 
 <!DOCTYPE html>
@@ -28,18 +30,18 @@ $eleve = array_filter($data['eleves'], function($student){
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
   <link href="assets/css/note.css" rel="stylesheet" />
-  <title><?= "John Doe" ?></title>
+  <title><?= $test['nom']; ?> <?= $test['prenom']; ?></title>
 </head>
 <body>
   <div class="page">
     <section class="student-card">
       <div class="student-photo">
-        <img src="" alt="Photo de John Doe">
+        <img src="assets/images/students/<?= $test["image"] ?>" alt="Photo de <?= $eleve["nom"]; ?> <?= $eleve["prenom"]; ?>">
       </div>
 
       <div class="student-info">
-        <h1>John Doe</h1>
-        <div class="student-class">B1</div>
+        <h1><?= $test["nom"]; ?> <?= $test["prenom"]; ?></h1>
+      <div class="student-class"><?= $test["classe"]; ?></div>
 
         <div class="student-summary">
           <div class="summary-box">
