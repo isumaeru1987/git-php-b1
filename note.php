b48cbb9dd8f8cd2fa27ec882f57756ca6fb491e0
ManonDufour8 <manon.dufour2608@gmail.com>
2026-04-02 10:41:00 +0200

    Récupération de l'id

--- note.php
+++ note.php
@@ -8,7 +8,7 @@ $data = json_decode(file_get_contents('data/students.json'), true);
 
 $eleve = array_filter($data['eleves'], function($student){
     //Remplacer 1 par la valeur passée dans l'URL
-    return $student['id'] == 1;
+    return $student['id'] == $_GET['id'];
 });
 
 ////Pour débugger :
