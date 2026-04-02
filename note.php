5f744f75834a24937983897e2d0e3e9f5481e419
Nighthaunt25 <louis.ganeau2611@gmail.com>
2026-04-02 09:54:21 +0200

    changement de valeur en dur et affichage note et nbMatière

--- note.php
+++ note.php
@@ -4,57 +4,77 @@ include 'index.php';
 $data = json_decode(file_get_contents($filepath), true);
 
 ////Pour débugger :
-//var_dump($data); //Affiche le tableau des eleves
+// var_dump($data); //Affiche le tableau des eleves
 //var_dump($GLOBALS); //Affiche les GET, POST, COOKIE, ...
 
-$eleve = array_filter($data['eleves'], function($student){
-    //Remplacer 1 par la valeur passée dans l'URL
-    return $student['id'] == $_GET["id"];
+$eleve = array_filter($data['eleves'], function ($student) {
+  //Remplacer 1 par la valeur passée dans l'URL
+  return $student['id'] == $_GET["id"];
 });
+$eleve = $eleve[0]
+  ////Pour débugger :
+// var_dump($eleve); //Affiche le tableau de l'élève
 
-////Pour débugger :
-//var_dump($eleve); //Affiche le tableau de l'élève
-
-?>
+  ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+    rel="stylesheet">
   <link href="assets/css/note.css" rel="stylesheet" />
   <title><?= "John Doe" ?></title>
 </head>
+
 <body>
   <div class="page">
     <section class="student-card">
       <div class="student-photo">
-        <img src="" alt="Photo de John Doe">
+        <img src="assets/images/students/<?= $eleve["image"]?>" alt="Photo de John Doe">
       </div>
 
       <div class="student-info">
-        <h1>John Doe</h1>
-        <div class="student-class">B1</div>
+        <h1><?= $eleve["nom"] . "." . $eleve["prenom"] ?>
+          <h1>
+            <div class="student-class"><?= $eleve["classe"] ?></div>
 
-        <div class="student-summary">
-          <div class="summary-box">
-            <div class="summary-label">Nombre de matières</div>
-            <div class="summary-value" id="nb-matieres">0</div>
-          </div>
+            <div class="student-summary">
+              <div class="summary-box">
+                <div class="summary-label">Nombre de matières</div>
+                <div class="summary-value" id="nb-matieres"><?= count($eleve["notes"]) ?></div>
+              </div>
 
-          <div class="summary-box">
-            <div class="summary-label">Nombre total de notes</div>
-            <div class="summary-value" id="nb-notes">0</div>
-          </div>
-
-          <div class="summary-box">
-            <div class="summary-label">Moyenne générale</div>
-            <div class="summary-value" id="moyenne-generale-top">15,00</div>
-          </div>
-        </div>
+              <div class="summary-box">
+                <div class="summary-label">Nombre total de notes</div>
+                <div class="summary-value" id="nb-notes">
+                  <?php $nbNote = 0;
+                  foreach ($eleve["notes"] as $value) {
+                    $nbNote += count($value);
+                  } 
+                  echo $nbNote;
+                  ?>
+                </div>
+              </div>
+              <div class="summary-box">
+                <div class="summary-label">Moyenne générale</div>
+                <div class="summary-value" id="moyenne-generale-top">
+                  <?php $moyenne = 0;
+                  foreach ($eleve["notes"] as $value) {
+                    foreach ($value as $note) {
+                      $moyenne += $note;
+                    }
+                  } 
+                  echo round($moyenne/$nbNote,2);
+                  ?>
+                </div>
+              </div>
+            </div>
       </div>
     </section>
 
@@ -72,14 +92,14 @@ $eleve = array_filter($data['eleves'], function($student){
           </thead>
           <tbody id="notes-body">
             <tr>
-                <td><strong>Maths</strong></td>
-                <td>
+              <td><strong>Maths</strong></td>
+              <td>
                 <div class="notes-list">
-                    <span class="note-badge">14/20</span>
-                    <span class="note-badge">16/20</span>
+                  <span class="note-badge">14/20</span>
+                  <span class="note-badge">16/20</span>
                 </div>
-                </td>
-                <td class="moyenne-cell">15,00/20</td>
+              </td>
+              <td class="moyenne-cell">15,00/20</td>
             </tr>
           </tbody>
         </table>
@@ -94,4 +114,5 @@ $eleve = array_filter($data['eleves'], function($student){
     </section>
   </div>
 </body>
+
 </html>
