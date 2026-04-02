f047ccdafb1c0e1ffd917b7a1169c34cea3b8abd
Nighthaunt25 <louis.ganeau2611@gmail.com>
2026-04-02 09:34:22 +0200

    fin index.php

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ $data = json_decode(file_get_contents($filepath), true);
                 <?= $value["evaluation_globale"] ?>
               </p>
               <div>
-                <button class="btn-notes">Voir les notes</button>
+                <a href="note.php?id=<?= $value["id"] ?>"><button class="btn-notes">Voir les notes</button></a>
               </div>
             </div>
           </article>
